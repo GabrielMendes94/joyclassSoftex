76c43e77d8f331940c5551175ddc208db93ce9f7
Gabriel Mendes <103916287+GabrielMendes94@users.noreply.github.com>
2023-10-11 08:48:19 +0000

    atv tipos

---
+++ cadastro.js
@@ -0,0 +1,7 @@
+//nome = string, salario = number, idade = number, possuiDiploma = boolean;
+let nome = "Severino";
+let idade = "40";
+let salario = 8.500;
+let possuiDiploma = true;
+
+console.log(`${typeof (nome)}, ${typeof (idade)}, ${typeof (salario)}, ${typeof (possuiDiploma)}`);
